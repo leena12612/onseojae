c060979edfa7e7f0180a18a6f34c21c5bf7b1e19
이나경 <sjmcl912@naver.com>
2026-07-07 16:55:22 +0900

    홈으로

--- client/src/pages/BookDetailPage.jsx
+++ client/src/pages/BookDetailPage.jsx
@@ -90,7 +90,7 @@ export default function BookDetailPage() {
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
-            검색으로 돌아가기
+            홈으로
           </Link>
           <ShareButton title={book?.title} />
         </div>
